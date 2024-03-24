451275ef093264e527d79bf4fb73073f18c2d321
Nam Nguyen <phuongnam@phuongnam.org>
2024-03-24 15:51:43 +0700

    add extensions table

---
+++ database/migrations/2024_03_24_084856_create_extensions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('extensions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('config')->nullable()->default(null);
+            $table->boolean('enabled')->default(true);
+            $table->boolean('is_default')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('extensions');
+    }
+};
