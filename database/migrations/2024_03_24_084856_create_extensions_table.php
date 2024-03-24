60c1a075c4213426f75d372d1a5653bb317237a8
Nam Nguyen <phuongnam@phuongnam.org>
2024-03-24 15:53:24 +0700

    update extensions table

--- database/migrations/2024_03_24_084856_create_extensions_table.php
+++ database/migrations/2024_03_24_084856_create_extensions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('extensions', function (Blueprint $table) {
+        Schema::create('admin_extensions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('extensions');
+        Schema::dropIfExists('admin_extensions');
     }
 };
