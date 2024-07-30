19c73eb7e825967f7f16ac467fecf47b6ac6faf1
phuongnamsoft <phuongnam@phuongnam.org>
2024-07-30 22:11:46 +0700

    add summernote

---
+++ src/Form/Field/Summernote.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PNS\Admin\Form\Field;
+
+use PNS\Admin\Form\Field\Textarea;
+
+class CKEditor extends Textarea
+{
+    protected $view = 'laravel-admin::summernote';
+
+    protected static $css = [
+        'vendor/laravel-admin/summernote/summernote.css',
+    ];
+
+    protected static $js = [
+        'vendor/laravel-admin/summernote/summernote.min.js',
+    ];
+
+    public function render()
+    {
+        $name = $this->formatName($this->column);
+
+        $config = (array) self::config('config');
+
+        $config = json_encode(array_merge([
+            'height' => 300,
+        ], $config));
+
+        $this->script = <<<EOT
+        (function(){
+            var configs = $config;
+
+            if(configs['imageUploadServer']){
+                configs.callbacks = configs.callbacks || {};
+                configs.callbacks.onImageUpload = function(images){
+                    window.laravelAdminSummernoteImageUploader($('#{$this->id}'),images[0],configs.imageUploadServer,configs.imageUploadName);
+                };
+            }
+
+            $('#{$this->id}').summernote(configs);
+
+            $('#{$this->id}').on("summernote.change", function (e) {
+                var html = $('#{$this->id}').summernote('code');
+                $('input[name="{$name}"]').val(html);
+            });
+        })();
+EOT;
+        
+        return parent::render();
+    }
+}
