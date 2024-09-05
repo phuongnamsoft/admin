3c0bc691e54d686d232c514336195c91ea2c3653
phuongnamsoft <phuongnam@phuongnam.org>
2024-09-05 22:15:15 +0700

    fix

--- src/Auth/Database/Extension.php
+++ src/Auth/Database/Extension.php
@@ -4,9 +4,14 @@ namespace PNS\Admin\Auth\Database;
 
 use PNS\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Database\Eloquent\Model;
+use PNS\Admin\Admin;
 
 class Extension extends Model
 {
+    const INSTALL_STATUS_NOT_INSTALLED = 0;
+    const INSTALL_STATUS_INSTALLED = 1;
+    const INSTALL_STATUS_INSTALL_FAILED = 2;
+
     use DefaultDatetimeFormat;
 
     /**
@@ -53,5 +58,28 @@ class Extension extends Model
     public function getMenuIdsAttribute($menuIds)
     {
         return is_string($menuIds) ? array_filter(array_map('intval', json_decode($menuIds))) : (array) $menuIds;
+    }
+
+    public function canInstall() {
+        return $this->install_status != self::INSTALL_STATUS_INSTALLED;
+    }
+
+    public function install() {
+        try {
+            $extension = Admin::$extensions[$this->slug];
+            $extension::import();
+
+            $this->enabled = 1;
+            $this->install_status = self::INSTALL_STATUS_INSTALLED;
+            $this->save();
+
+            return true;
+        } catch (\Throwable $th) {
+            $this->install_logs = $th->getMessage();
+            $this->install_status = self::INSTALL_STATUS_INSTALL_FAILED;
+            //throw $th;
+
+            return false;
+        }
     }
 }
