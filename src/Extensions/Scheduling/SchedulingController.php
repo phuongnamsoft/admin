f3c7531e068f76cd0957d1e4509de3579894fc49
phuongnamsoft <phuongnam@phuongnam.org>
2024-09-05 21:32:18 +0700

    add schedule

---
+++ src/Extensions/Scheduling/SchedulingController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PNS\Admin\Extensions\Scheduling;
+
+use PNS\Admin\Facades\Admin;
+use PNS\Admin\Layout\Content;
+use Illuminate\Http\Request;
+
+class SchedulingController
+{
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('Task scheduling');
+
+            $scheduling = new Scheduling();
+
+            $content->body(view('laravel-admin-scheduling::index', [
+                'events' => $scheduling->getTasks(),
+            ]));
+        });
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function runEvent(Request $request)
+    {
+        $scheduling = new Scheduling();
+
+        try {
+            $output = $scheduling->runTask($request->get('id'));
+
+            return [
+                'status'    => true,
+                'message'   => 'success',
+                'data'      => $output,
+            ];
+        } catch (\Exception $e) {
+            return [
+                'status'    => false,
+                'message'   => 'failed',
+                'data'      => $e->getMessage(),
+            ];
+        }
+    }
+}
